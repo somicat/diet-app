8739403267e7b288e0cf03cea721231c453e939b
somicat <hellencat0403@gmail.com>
2026-05-21 11:35:15 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from 'react';
 import { Home, Utensils, Scale, Dumbbell, Plus, X, Activity, ArrowUp, ArrowDown, Database, Check, ChevronLeft, ChevronRight, Droplets, Calendar, Trash2, Settings, Edit3 } from 'lucide-react';
 
